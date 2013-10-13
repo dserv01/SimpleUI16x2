be73272e02d6dd75dd3865233b80536327e9147d
dserv01 <d.krupke@tu-bs.de>
2013-10-13 13:55:41 +0200

    gpl

--- SimpleUI16x2.h
+++ SimpleUI16x2.h
@@ -1,4 +1,21 @@
 /**
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Dominik Krupke (dk[at]dserv01.de), dserv01.de, 2013
+ */
+
+/**
  * This library provides a simple User Interface for Input and Output with an 
  * 16x2 Character Display and some Buttons. Some examples:
  * 
